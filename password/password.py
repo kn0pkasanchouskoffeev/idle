56ef435b68ddb9389a75a69273440bcf3a3aacb6
kn0pkasanchouskoffeev <72977363+kn0pkasanchouskoffeev@users.noreply.github.com>
2020-11-15 16:16:36 +0700

    Create password.py

---
+++ password/password.py
@@ -0,0 +1,6 @@
+password = input("введите пароль")
+while password != "apple":
+    print("неверный пароль. подсказка: вкусная компания")
+    password = input("введите пароль")
+print("верный пароль")
+        
